e850690b90e0834999281540802d7bed13287129
intantunti15 <127914172+intantunti15@users.noreply.github.com>
2023-03-24 12:40:18 +0800

    upload

---
+++ intan(2201010393)/sesi2/latihan2.php
@@ -0,0 +1,9 @@
+<?php
+ echo "<h3>Latihan kondisi pada PHP</h3>";
+ $angka= 6;
+
+ if($angka > 3) {
+    echo"Nilai angka lebih dari 3";
+ }else{
+     echo "Nilai angka kurang dari atau sama dengan 3";
+ }
